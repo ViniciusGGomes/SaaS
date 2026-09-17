783d9ccbded40ebac6d1f2dedeac1f9c62347bff
vinicius <viniciusgmelllo@gmail.com>
2026-09-16 21:24:14 -0300

    feat(api): implement global error handler for zod, custom domain errors, and internal server errors

---
+++ apps/api/src/http/error-handler.ts
@@ -0,0 +1,33 @@
+import type { FastifyInstance } from 'fastify'
+import { ZodError } from 'zod'
+import { BadRequestError } from './routes/_errors/bad-request-error'
+import { UnauthorizedError } from './routes/_errors/unauthorized-error'
+
+type FastifyErrorHandle = FastifyInstance['errorHandler']
+
+export const errorHandle: FastifyErrorHandle = (error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      errors: error.flatten().fieldErrors,
+    })
+  }
+
+  if (error instanceof BadRequestError) {
+    return reply.status(400).send({
+      message: error.message,
+    })
+  }
+
+  if (error instanceof UnauthorizedError) {
+    return reply.status(401).send({
+      message: error.message,
+    })
+  }
+
+  console.log(error)
+
+  // send error to soem observability platform
+
+  return reply.status(500).send({ message: 'internal server error.' })
+}
