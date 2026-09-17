e5998ed61dfccc6634404c925e298423090d133d
vinicius <viniciusgmelllo@gmail.com>
2026-09-16 21:29:36 -0300

    feat(api): implement password authentication route (/sessions/password)

---
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -0,0 +1,69 @@
+import { prisma } from '@/lib/prisma'
+import { FastifyInstance } from 'fastify'
+import { type ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { compare } from 'bcryptjs'
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function authenticateWithPassword(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/sessions/password',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Authenticate with e-mail & password',
+        body: z.object({
+          email: z.email(),
+          password: z.string().min(6),
+        }),
+        response: {
+          201: z.object({
+            token: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email, password } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!userFromEmail) {
+        throw new BadRequestError('Invalid credentials')
+      }
+
+      // Se o usuário só loga pelo Github
+      if (userFromEmail.passwordHash === null) {
+        throw new BadRequestError(
+          'User does not have a password, use social login.'
+        )
+      }
+
+      const isPasswordValid = await compare(
+        password,
+        userFromEmail.passwordHash
+      )
+
+      if (!isPasswordValid) {
+        throw new BadRequestError('Invalid credentials')
+      }
+
+      const token = await reply.jwtSign(
+        {
+          sub: userFromEmail.id,
+        },
+        {
+          sign: {
+            expiresIn: '7d',
+          },
+        }
+      )
+
+      return reply.status(201).send({ token })
+    }
+  )
+}
