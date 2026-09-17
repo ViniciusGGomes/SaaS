58970f4d49eefcd926e06f9c866a4cc01e13c82d
vinicius <viniciusgmelllo@gmail.com>
2026-09-16 21:29:11 -0300

    feat(api): implement user registration route (/users)

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,64 @@
+import { prisma } from '@/lib/prisma'
+import { FastifyInstance } from 'fastify'
+import { type ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { hash } from 'bcryptjs'
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function createAccount(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/users',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Create a new account',
+        body: z.object({
+          name: z.string(),
+          email: z.email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        throw new BadRequestError('user with same e-mail already exist')
+      }
+
+      const [, domain] = email.split('@')
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        },
+      })
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+          member_on: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                },
+              }
+            : undefined,
+        },
+      })
+
+      return reply.status(201).send()
+    }
+  )
+}
