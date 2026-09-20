f5ec7e26a3c37174e4b4ae5b478d265f365156eb
vinicius <viniciusgmelllo@gmail.com>
2026-09-20 14:25:43 -0300

    add getUserMembership interface

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,11 @@
+import { Membership, Organization } from '@prisma/client'
 import 'fastify'
 
 declare module 'fastify' {
   export interface FastifyRequest {
     getCurrentUserId(): Promise<string>
+    getUserMembership(
+      slug: string
+    ): Promise<{ organization: Organization; membership: Membership }>
   }
 }
