282ca98677e75ab6d1afb232e45ef22fa176a55e
vinicius <viniciusgmelllo@gmail.com>
2026-09-20 19:28:43 -0300

    feat(api): register organization, project and member routes in server

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,20 @@ import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
 import { authenticateWithGoogle } from './routes/auth/authenticate-with-google'
 import { env } from '@saas/env'
 import { createOrganization } from './routes/orgs/create-organization'
+import { getMembership } from './routes/orgs/get-membership'
+import { getOrganization } from './routes/orgs/get-organization'
+import { getOrganizations } from './routes/orgs/get-organizations'
+import { updateOrganization } from './routes/orgs/update-organization'
+import { shutdownOrganization } from './routes/orgs/shutdown-organization'
+import { transferOrganization } from './routes/orgs/transfer-organization'
+import { createProject } from './routes/projects/create-project'
+import { deleteProject } from './routes/projects/delete-project'
+import { getProject } from './routes/projects/get-project'
+import { getProjects } from './routes/projects/get-projects'
+import { updateProject } from './routes/projects/update-project'
+import { getMembers } from './routes/members/get-members'
+import { updateMember } from './routes/members/update-member'
+import { removeMember } from './routes/members/remove-member'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -53,7 +67,7 @@ app.register(fastifyJwt, {
 })
 
 app.register(fastifyCors)
-
+/* Auth */
 app.register(createAccount)
 app.register(authenticateWithPassword)
 app.register(authenticateWithGithub)
@@ -62,7 +76,26 @@ app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
 
+/* Organizations */
 app.register(createOrganization)
+app.register(getMembership)
+app.register(getOrganization)
+app.register(getOrganizations)
+app.register(updateOrganization)
+app.register(shutdownOrganization)
+app.register(transferOrganization)
+
+/* Projects */
+app.register(createProject)
+app.register(deleteProject)
+app.register(getProject)
+app.register(getProjects)
+app.register(updateProject)
+
+/* Members */
+app.register(getMembers)
+app.register(updateMember)
+app.register(removeMember)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server is running')
