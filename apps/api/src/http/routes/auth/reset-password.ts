745f971ac3af365c0626ff6adfc2946baf61c5a3
vinicius <viniciusgmelllo@gmail.com>
2026-09-21 19:07:47 -0300

    feat(api): invalidate recovery token after password reset

--- apps/api/src/http/routes/auth/reset-password.ts
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -5,6 +5,7 @@ import { z } from 'zod'
 import { auth } from '@/http/middlewares/auth'
 import { UnauthorizedError } from '../_errors/unauthorized-error'
 import { hash } from 'bcryptjs'
+import { codec } from 'zod/mini'
 
 export async function resetPassword(app: FastifyInstance) {
   app
@@ -40,14 +41,22 @@ export async function resetPassword(app: FastifyInstance) {
 
         const passwordHash = await hash(password, 6)
 
-        await prisma.user.update({
-          where: {
-            id: tokenFromCode.userId,
-          },
-          data: {
-            passwordHash,
-          },
-        })
+        await prisma.$transaction([
+          prisma.user.update({
+            where: {
+              id: tokenFromCode.userId,
+            },
+            data: {
+              passwordHash,
+            },
+          }),
+
+          prisma.token.delete({
+            where: {
+              id: code,
+            },
+          }),
+        ])
 
         return reply.status(204).send()
       }
