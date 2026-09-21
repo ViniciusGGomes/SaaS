0161a28353b713930b70059e846e8ad24d8d156c
vinicius <viniciusgmelllo@gmail.com>
2026-09-21 19:02:11 -0300

    feat(api): add get user pending invites route

---
+++ apps/api/src/http/routes/invites/get-pending-invites.ts
@@ -0,0 +1,89 @@
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { BadRequestError } from '../_errors/bad-request-error'
+import { Role } from '@prisma/client'
+
+export async function getPendingInvites(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/pending-invites',
+      {
+        schema: {
+          tags: ['Invites'],
+          summary: 'Get all user pending invites',
+          security: [{ bearerAuth: [] }],
+          response: {
+            200: z.object({
+              invites: z.array(
+                z.object({
+                  id: z.uuid(),
+                  role: z.enum(Role),
+                  email: z.email(),
+                  createdAt: z.date(),
+                  author: z
+                    .object({
+                      id: z.uuid(),
+                      name: z.string().nullable(),
+                      avatarUrl: z.url().nullable(),
+                    })
+                    .nullable(),
+                  organization: z.object({
+                    name: z.string(),
+                  }),
+                })
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+
+        const user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        })
+
+        if (!user) {
+          throw new BadRequestError('User not found')
+        }
+
+        const invites = await prisma.invite.findMany({
+          where: {
+            email: user.email,
+          },
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            author: {
+              select: {
+                id: true,
+                name: true,
+                avatarUrl: true,
+              },
+            },
+            organization: {
+              select: {
+                name: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        })
+
+        return reply.status(200).send({
+          invites,
+        })
+      }
+    )
+}
